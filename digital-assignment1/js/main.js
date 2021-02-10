f4565f4da2a538c723f24c3a65420b009e309eee
Orlando <omiguel@gmu.edu>
2021-02-10 04:56:57 -0500

    no class gang

--- digital-assignment1/js/main.js
+++ digital-assignment1/js/main.js
@@ -1,4 +1,4 @@
-import "./phaser.js";
+"use strict";
 
 // You can copy-and-paste the code from any of the examples at https://examples.phaser.io here.
 // You will need to change the `parent` parameter passed to `new Phaser.Game()` from
@@ -9,49 +9,40 @@ import "./phaser.js";
 // loading functions to reflect where you are putting the assets.
 // All loading functions will typically all be found inside `preload()`.
 
-// The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
+function preload() {
+    // Load an image and call it 'logo'.
+    this.load.image( 'logo', 'assets/phaser.png' );
+}
 
-class MyScene extends Phaser.Scene {
+let bouncy;
+
+function create() {
+    // Create a sprite at the center of the screen using the 'logo' image.
+    bouncy = this.physics.add.sprite( this.cameras.main.centerX, this.cameras.main.centerX, 'logo' );
     
-    constructor() {
-        super();
-        
-        this.bouncy = null;
-    }
+    // Make it bounce off of the world bounds.
+    bouncy.body.collideWorldBounds = true;
     
-    preload() {
-        // Load an image and call it 'logo'.
-        this.load.image( 'logo', 'assets/nugget.png' );
-    }
+    // Make the camera shake when clicking/tapping on it.
+    bouncy.setInteractive();
+    bouncy.on( 'pointerdown', function( pointer ) {
+        this.scene.cameras.main.shake(500);
+        });
     
-    create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        this.bouncy = this.physics.add.sprite( this.cameras.main.centerX, this.cameras.main.centerX, 'logo' );
-        
-        // Make it bounce off of the world bounds.
-        this.bouncy.body.collideWorldBounds = true;
-        
-        // Make the camera shake when clicking/tapping on it.
-        this.bouncy.setInteractive();
-        this.bouncy.on( 'pointerdown', function( pointer ) {
-            this.scene.cameras.main.shake(500);
-            });
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        let text = this.add.text( this.cameras.main.centerX, 15, "Hello, Orlando.", style );
-        text.setOrigin( 0.5, 0.0 );
-    }
-    
-    update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        this.bouncy.rotation = this.physics.accelerateToObject( this.bouncy, this.input.activePointer, 500, 500, 500 );
-    }
+    // Add some text using a CSS style.
+    // Center it in X, and position its top 15 pixels from the top of the world.
+    let style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+    let text = this.add.text( this.cameras.main.centerX, 15, "Build something amazing.", style );
+    text.setOrigin( 0.5, 0.0 );
+}
+
+function update() {
+    // Accelerate the 'logo' sprite towards the cursor,
+    // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
+    // in X or Y.
+    // This function returns the rotation angle that makes it visually match its
+    // new trajectory.
+    bouncy.rotation = this.physics.accelerateToObject( bouncy, this.input.activePointer, 500, 500, 500 );
 }
 
 const game = new Phaser.Game({
@@ -59,6 +50,7 @@ const game = new Phaser.Game({
     parent: 'game',
     width: 800,
     height: 600,
-    scene: MyScene,
+    // Phaser converts a dictionary of methods into a Scene subclass.
+    scene: { preload: preload, create: create, update: update },
     physics: { default: 'arcade' },
     });
